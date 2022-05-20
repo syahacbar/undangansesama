192ad3072e45aa0ca2fd9087e033ebfca09358c5
syahacbar <syahacbar@gmail.com>
2022-05-20 13:39:02 +0900

    Update cetak.php

--- application/views/cetak.php
+++ application/views/cetak.php
@@ -15,7 +15,7 @@
 <table border="0" cellspacing=0 cellpadding=0>
 	<tr>
 		<td rowspan="5" width="15%"><img src="'.base_url().'/assets/unipa-warna.png" width="70" height="70"></td>
-		<td align="center" width="75%"><font style="font-family:times" size="14"><b>KEMENTERIAN PENDIDIKAN DAN KEBUDAYAAN</b></font></td>
+		<td align="center" width="75%"><font style="font-family:times" size="14"><b>KEMENTERIAN PENDIDIKAN, KEBUDAYAAN, RISET, DAN TEKNOLOGI</b></font></td>
 		<td rowspan="5" width="10%"></td>
 	</tr>
 	<tr>
